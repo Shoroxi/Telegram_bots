e990aaf01bb6d2e3bd396d23168b0a5c70d2431d
Шкарупа Глеб <40485962+Shoroxi@users.noreply.github.com>
2022-06-03 23:06:29 +0300

    Update config.py

--- DZ_bot/config/config.py
+++ DZ_bot/config/config.py
@@ -1,9 +1,3 @@
-__telegramtoken__ = "5201655115:AAFbPgpEaPMThZCUETrvHZiHjqqnAbx1MoI"
+__telegramtoken__ = ""
 __admin_id__ = 818019160 #@Shorox
-__database_url__ = "postgres://npvrhjnqwkmpot:5b7af1175bfef69a71736bbaf2cb066a5725ed7c35e4e2d21a32c8b02941e7aa@ec2-3-211-221-185.compute-1.amazonaws.com:5432/d160c4u37oti4g"
-
-# __host__ = "ec2-3-211-221-185.compute-1.amazonaws.com"
-# __database__ = "d160c4u37oti4g"
-# __user__ = "npvrhjnqwkmpot"
-# __port__ = 5432
-# __password__ = "5b7af1175bfef69a71736bbaf2cb066a5725ed7c35e4e2d21a32c8b02941e7aa"
+__database_url__ = ""
